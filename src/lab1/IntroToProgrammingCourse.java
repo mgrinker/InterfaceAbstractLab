d271a2f61a0e194383d5a47b0d9759420714d3ff
mgrinker <mgrinker@L11707038601.wctc.msft>
2013-01-31 17:03:05 -0600

    Modified AdvancedJavaCourse class deleted getters and added overide setters

--- src/lab1/IntroToProgrammingCourse.java
+++ src/lab1/IntroToProgrammingCourse.java
@@ -10,29 +10,18 @@ import javax.swing.JOptionPane;
  * @version     1.00
  */
 public class IntroToProgrammingCourse extends CollegeCourse{
+
+    public IntroToProgrammingCourse(String courseName, String courseNumber, double credits, String prerequisites, String instructor, String roomNumber, String daysOfWeek, String classTime) {
+        super(courseName, courseNumber, credits, prerequisites, instructor, roomNumber, daysOfWeek, classTime);
+    }
     
-
-    public IntroToProgrammingCourse(String courseName, String courseNumber) {
-        this.setCourseName(courseName);
-        this.setCourseNumber(courseNumber);
-    }
-
-    @Override
-    public String getCourseNumber() {
-        return courseNumber;
-    }
-
     public final void setCourseNumber(String courseNumber) {
         if(courseNumber == null || courseNumber.length() == 0) {
             JOptionPane.showMessageDialog(null,
                     "Error: courseNumber cannot be null of empty string");
             System.exit(0);
         }
-        this.courseNumber = courseNumber;
-    }
-
-    public double getCredits() {
-        return credits;
+        
     }
 
     public void setCredits(double credits) {
@@ -41,11 +30,6 @@ public class IntroToProgrammingCourse extends CollegeCourse{
                     "Error: credits must be in the range 0.5 to 4.0");
             System.exit(0);
         }
-        this.credits = credits;
-    }
-
-    public String getCourseName() {
-        return courseName;
     }
 
     public final void setCourseName(String courseName) {
@@ -54,8 +38,31 @@ public class IntroToProgrammingCourse extends CollegeCourse{
                     "Error: courseName cannot be null of empty string");
             System.exit(0);
         }
-        this.courseName = courseName;
     }
+    
+    public void setPrerequisites(String prerequisites) {
+        
+    }
+    
+    public void setInstructor(String instructor) {
+        
+    }
+    
+    public void setRoomNumber(String roomNumber) {
+        
+    }
+    
+    public void setDaysOfWeek(String daysOfWeek) {
+        
+    }
+    
+    public void setClassTime(String classTime) {
+        
+    }
+    
+    
+    
+    
 
     
 }
