592452b6e53191889869758d39758a50b8c716cf
Michael <Michael@Control>
2013-02-02 13:57:37 -0600

    Copied from introToProgramming to advanced Java and modified for advanced Java class

--- src/lab1/AdvancedJavaCourse.java
+++ src/lab1/AdvancedJavaCourse.java
@@ -1,65 +1,111 @@
 package lab1;
 
-import javax.swing.JOptionPane;
-
 /**
  * Describe responsibilities here.
  *
  * @author      Michael Grinker
  * @version     1.00
  */
-public class AdvancedJavaCourse {
-    String courseName;
-    private String courseNumber;
-    private double credits;
-    private String prerequisites;
-
-    public AdvancedJavaCourse(String courseName, String courseNumber) {
-        this.setCourseName(courseName);
-        this.setCourseNumber(courseNumber);
-    }
-
-    public String getCapitalizedCourseName() {
-        return this.getCourseName().toUpperCase();
-    }
-
-
-    public String getPrerequisites() {
-        return prerequisites;
-    }
-
-    public void setPrerequisites(String prerequisites) {
-        if(prerequisites == null || prerequisites.length() == 0) {
-            JOptionPane.showMessageDialog(null,
-                    "Error: prerequisites cannot be null of empty string");
-            System.exit(0);
-        }
-        this.prerequisites = prerequisites;
-    }
-
-    public void setCredits(double credits) {
-        if(credits < 0.5 || credits > 4.0) {
-            JOptionPane.showMessageDialog(null,
-                    "Error: credits must be in the range 0.5 to 4.0");
-            System.exit(0);
-        }
-        this.setCredits(credits);
-    }
-
-    public String getCourseName() {
-        return courseName;
-    }
-
-    public void setCourseName(String courseName) {
-        this.courseName = courseName;
-    }
-
-    public String getCourseNumber() {
-        return courseNumber;
-    }
-
-    public void setCourseNumber(String courseNumber) {
-        this.courseNumber = courseNumber;
-    }
+public class AdvancedJavaCourse extends CollegeCourse {
+    private final String COURSE_NAME = "Advanced Java";
+    private final String COURSE_NUMBER = "152-134";
+    private final double CREDITS = 4;
+    private final String PREREQUISITES = "Intro to Java";
+    private final double TUITION = 508.60;
+    private String sectionNumber;
+    private String instructor;
+    private String roomNumber;
+    private String dates;
+    private String daysOfWeek;
+    private String classTime;
+    private int numberOfSeats;
     
+    public AdvancedJavaCourse() {
+    }
+
+    public AdvancedJavaCourse(String sectionNumber, String instructor, String roomNumber, String dates, String daysOfWeek, String classTime, int numberOfSeats) {
+        this.sectionNumber = sectionNumber;
+        this.instructor = instructor;
+        this.roomNumber = roomNumber;
+        this.dates = dates;
+        this.daysOfWeek = daysOfWeek;
+        this.classTime = classTime;
+        this.numberOfSeats = numberOfSeats;
+    }
+
+    public String getSectionNumber() {
+        return sectionNumber;
+    }
+
+    public void setSectionNumber(String sectionNumber) {
+        this.sectionNumber = sectionNumber;
+    }
+
+    public String getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(String instructor) {
+        this.instructor = instructor;
+    }
+
+    public String getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(String roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public String getDates() {
+        return dates;
+    }
+
+    public void setDates(String dates) {
+        this.dates = dates;
+    }
+
+    public String getDaysOfWeek() {
+        return daysOfWeek;
+    }
+
+    public void setDaysOfWeek(String daysOfWeek) {
+        this.daysOfWeek = daysOfWeek;
+    }
+
+    public String getClassTime() {
+        return classTime;
+    }
+
+    public void setClassTime(String classTime) {
+        this.classTime = classTime;
+    }
+
+    public int getNumberOfSeats() {
+        return numberOfSeats;
+    }
+
+    public void setNumberOfSeats(int numberOfSeats) {
+        this.numberOfSeats = numberOfSeats;
+    }
+
+    public String getCOURSE_NAME() {
+        return COURSE_NAME;
+    }
+
+    public String getCOURSE_NUMBER() {
+        return COURSE_NUMBER;
+    }
+
+    public double getCREDITS() {
+        return CREDITS;
+    }
+
+    public String getPREREQUISITES() {
+        return PREREQUISITES;
+    }
+
+    public double getTUITION() {
+        return TUITION;
+    }
 }
