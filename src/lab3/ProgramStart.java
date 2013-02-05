db8cf4f93f9c34df70ec82e40769f5fdf46f5b69
Michael <Michael@Control>
2013-02-05 14:40:13 -0600

    Program Start with comments

---
+++ src/lab3/ProgramStart.java
@@ -0,0 +1,22 @@
+
+package lab3;
+
+/**
+ *
+ * @author Michael
+ */
+public class ProgramStart {
+    
+    public static void main(String[] args) {
+        
+        //Have an abstract class which would hold the general school information
+        // Having a hard time figuring out the "is a" relationship
+        // A student "is a(n)" enrollee, but could you also have a class below
+        // the students for registration?  "Is a" registration a student?
+        //Have seperate interfaces for a undergrad and a graduate because
+        // the graduate would have more information as to when and where they
+        //received their original degree.  The course information should be in a
+        // seperate class, but it wouldn't be inherited? 
+    }
+    
+}
